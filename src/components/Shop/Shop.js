c10a64bb0c1cc7bf2b041d75b431597a1b4f8855
alnasimul <alnasimul@gmail.com>
2020-04-27 23:41:46 +0600

    shop page setup is done

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,16 +1,33 @@
 import React, { useState } from 'react';
 import fakeData from '../../fakeData';
+import './Shop.css'
+import Product from '../Product/Product';
+import Cart from '../Cart/Cart';
 
 const Shop = () => {
-    const first10 = fakeData.slice(0,10);
-    const [products,setProducts] = useState(first10);
+    const first10 = fakeData.slice(0, 10);
+    const [products, setProducts] = useState(first10);
+    const [cart, setCart] = useState([]);
+    function handelAddProduct(product) {
+        console.log(product)
+        console.log("product added");
+        //console.log(cart);
+        const newCart = [...cart,product];
+        setCart(newCart)
+        // const newCart = [...cart,product];
+        // console.log(newCart);
+    }
     return (
-        <div>
-            <h1>This is Shop</h1>
-            <ul>
-                {console.log(products)}
-                {products.map(product => <li>{product.name}</li>)}
-            </ul>
+        <div class="shop-container">
+            <div className="product-container">
+
+                {/* {console.log(products)} */}
+                {products.map(pd => <Product product={pd} handelAddProduct={handelAddProduct}>{pd.name}</Product>)}
+
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}></Cart>
+            </div>
         </div>
     );
 };
